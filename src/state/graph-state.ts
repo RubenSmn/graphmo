c6a5effaf534ab8feabdc990a49ef277e3a0895a
RubenSmn <saaman.ruben@gmail.com>
2022-06-17 22:36:18 +0200

    feat(state): create from graph with mock state

--- src/state/graph-state.ts
+++ src/state/graph-state.ts
@@ -4,4 +4,27 @@ export class GraphState extends BaseState {
   constructor() {
     super();
   }
+
+  /**
+   * Create state from graph
+   */
+  public static fromGraph(graph: { [key: string]: string[] }) {
+    const nodes = [];
+    const conns = [];
+
+    for (const key in graph) {
+      nodes.push(key);
+      for (const neighbor of graph[key]) {
+        const conn = key + ',' + neighbor;
+        if (
+          conns.indexOf(conn) > -1 ||
+          conns.indexOf(neighbor + ',' + key) > -1
+        )
+          continue;
+        conns.push(conn);
+      }
+    }
+
+    return { nodes, conns };
+  }
 }
